a03ee6994762514243c4cde4f74cdefe797cd356
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-06-30 05:43:26 +0600

    addEvents and events and home components added

---
+++ src/components/AddEvents/AddEvents.js
@@ -0,0 +1,10 @@
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const AddEvents = () => {
+  return <div></div>;
+};
+
+export default AddEvents;
